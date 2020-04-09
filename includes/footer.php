9c0207516c11fe1886279c5e044ad32021064cd2
Sibanye <sibanyebukani01@gmail.com>
2020-04-09 12:22:19 +0200

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,134 @@
+    <!-- #footer Begin -->
+    <footer id="footer">
+        <!-- container Begin -->
+        <div class="container justify-content-center text-center">
+            <!-- row Begin -->
+            <div class="row text-center">
+                <!-- col-sm-6 col-md-3 col-lg-3 Begin -->
+                <div class="col-sm-6 col-md-3 col-lg-3">
+                <h4>Pages</h4>
+                <!-- ul Begin -->
+                <ul>
+                    <li><a href="cart.php">Shopping Cart</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="checkout.php">My Account</a></li>
+                </ul>
+                <!-- ul Finish -->
+                <hr>
+
+                <h4>User Section</h4>
+                <!-- ul Begin -->
+                <ul>
+                    <li><a href="checkout.php">Login</a></li>
+                    <li><a href="customer_register.php">Register</a></li>
+                </ul>
+                <!-- ul Finish -->
+
+                <hr class=".d-none">
+
+                </div>
+                <!-- col-sm-6 col-md-3 col-lg-3 Begin -->
+
+                <!-- col-sm-6 col-md-3 col-lg-3 Begin -->
+                <div class="com-sm-6 col-md-3 col-lg-3 ">
+                <h4>Top Categories</h4>
+                <!-- ul Begin -->
+                <ul>
+                    <li><a href="#">Jackets</a></li>
+                    <li><a href="#">Accessories</a></li>
+                    <li><a href="#">Coats</a></li>
+                    <li><a href="#">Shoes</a></li>
+                    <li><a href="#">T-Shirts</a></li>
+                </ul>
+                <!-- ul Finish -->
+                <hr class="d-md-none .d-lg-none">
+            </div>
+            <!-- col-sm-6 col-md-3 col-lg-3 Finish -->
+            <!-- col-sm-6 col-md-3 col-lg-3 Begin -->
+            <div class="col-sm-6 col-md-3 col-lg-3">
+                <h4>Find Us</h4>
+                <!-- p Start -->
+                <p>
+                    <strong>Kasi Mall Online</strong>
+                    <br/>Vanderbiljpark
+                    <br/>063-930-5623
+                    <br/>kasimaill.co.za
+                    <br/><strong>Sibanye Bukani</strong>
+                </p>
+                <!-- p Finish -->
+                <a href="contact.php">Check Our Contact Page</a>
+                <hr class="d-md-none .d-lg-none">
+            </div>
+            <!-- col-sm-6 col-md-3 col-lg-3 Finish -->
+            <!-- col-sm-6 col-md-3 col-lg-3 Finish -->
+            <div class="col-sm-6 col-md-3 col-lg-3">
+            <h4>Get The News</h4>
+                <p>Don't miss our latest updates.</p>
+                <!-- form begin -->
+                <form action="" method="post">
+                    <!-- input-group begin -->
+                    <div class="input-group">
+                        
+                        <input type="text" class="form-control" name="email">
+                        <!-- input-group-btn begin -->
+                        <span class="input-group-btn">
+                            <input type="submit" value="subscribe" class="btn btn-default">
+                        </span>
+                        <!-- input-group-btn Finish -->
+                    </div>
+                    <!-- input-group Finish -->
+                </form>
+                <!-- form Finish -->
+
+                <hr>
+                
+                <h4>Keep In Touch</h4>
+                
+                <p class="social">
+                    <a href="#" class="icon1 fab  fa-facebook"></a>
+                    <a href="#" class="icon1 fab fa-twitter"></a>
+                    <a href="#" class="icon1 fab fa-instagram"></a>
+                </p>
+
+            </div>
+            <!-- col-sm-6 col-md-3 col-lg-3 End -->
+
+            </div>
+            <!-- row End -->
+        </div>
+        <!-- container End -->
+    </footer>
+    <!-- #footer End -->
+    <!-- #copyright Begin -->
+    <section id="copyright">
+    <!-- container Begin -->
+    <div class="container">
+        <!-- col-md-6 Begin -->
+        <div class="col-md-6">
+            <p class="pull-left">&copy; 2020 BukaniTechSolutions All Rights Reserve</p>
+        </div>
+        <!-- col-md-6 Finish -->
+        <!-- col-md-6 Begin -->
+        <div class="col-md-6">
+            <p class="pull-right">Theme by: <a href="#">Sibanye Bukani</a></p>
+        </div>
+        <!-- col-md-6 Finish -->
+    </div>
+    <!-- container Finish -->
+</section>
+<!-- #copyright Finish -->
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
